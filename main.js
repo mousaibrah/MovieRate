0fb55684aee04c0387b7d3bcfb5d013ba16bfafa
mousaibrah <mousaKibrahim@gmail.com>
2023-01-29 09:04:03 +0300

    Project 2

--- main.js
+++ main.js
@@ -31,7 +31,18 @@ const searchInput = $(`.search-input`)
 ;
 let favElements = [];
 // Data Variable
-// fetch(themoviedb.orgmovieapi_key=fccd45168523a14a8e5316ad86dca398)
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '2202988c9dmsh2ec8253a5536243p13f647jsn32c878fac3be',
+// 		'X-RapidAPI-Host': 'imdb-top-100-movies.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://imdb-top-100-movies.p.rapidapi.com/', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
 
 const movieData = [
   { title: "Wrath Of Man",
